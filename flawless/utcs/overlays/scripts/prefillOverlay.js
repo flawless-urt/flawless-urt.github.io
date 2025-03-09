b89e3bd7e7daef67ae0ece3939a8826606a24100
mryeah <9140406+mryay@users.noreply.github.com>
2025-03-10 00:23:10 +0100

    add script to prefill season values

---
+++ flawless/utcs/overlays/scripts/prefillOverlay.js
@@ -0,0 +1,7 @@
+﻿function prefillOverlay() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const seasons = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementsByName('season')[0].value = seasons[month] + ' ' + year;
+}
